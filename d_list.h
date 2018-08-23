e804a6ffca40840637ee003484c2bac6bb3aab32
Philip R. Simonson <drunkenmaster1988@gmail.com>
2018-08-23 10:46:33 -0400

    Fixed my d_list and added my c89 debug library

--- d_list.h
+++ d_list.h
@@ -37,149 +37,154 @@ enum DLIST_STATE { DLIST_STATE_START, DLIST_STATE_NEXT, DLIST_STATE_END };
 
 /* --------- don't touch these definitions --------- */
 
-enum DLIST_STATE _dlist_state = DLIST_STATE_START;
-struct DLIST *_dlist_last_node = NULL;
-struct DLIST *_dlist_last_saved = NULL;
+static enum DLIST_STATE _dlist_state = DLIST_STATE_START;
+static struct DLIST *_dlist_last_node = NULL;
+static struct DLIST *_dlist_last_saved = NULL;
 
-add_data_cb dlist_add_data = NULL;
-remove_data_cb dlist_remove_data = NULL;
-prepend_node_cb dlist_prepend_node = NULL;
-append_node_cb dlist_append_node = NULL;
+static add_data_cb dlist_add_data = NULL;
+static remove_data_cb dlist_remove_data = NULL;
+static prepend_node_cb dlist_prepend_node = NULL;
+static append_node_cb dlist_append_node = NULL;
+
+extern char *strdup(const char *);
 
 /* ------------ End of Definitions ----------------- */
 
-static void *_dlist_add_data(int num, const char *msg);
-static void _dlist_remove_data(void **dat);
-static void _dlist_prepend_node(void **root, int num, const char *msg);
-static void _dlist_append_node(void **root, int num, const char *msg);
-
-static void dlist_init(add_data_cb add_func,
-                    remove_data_cb remove_func,
-                    prepend_node_cb prepend_func,
-                    append_node_cb append_func)
-{
-    dlist_add_data = (add_func) ? add_func : _dlist_add_data;
-    dlist_remove_data = (remove_func) ? remove_func : _dlist_remove_data;
-    dlist_prepend_node = (prepend_func) ? prepend_func : _dlist_prepend_node;
-    dlist_append_node = (append_func) ? append_func : _dlist_append_node;
-}
-
 static void dlist_iterator_init(struct DLIST *root)
 {
-    _dlist_last_node = (root) ? root : NULL;
-    _dlist_last_saved = NULL;
-    _dlist_state = DLIST_STATE_START;
+	_dlist_last_node = (root) ? root : NULL;
+	_dlist_last_saved = NULL;
+	_dlist_state = DLIST_STATE_START;
 }
 
 static void dlist_iterator_reset(void)
 {
-    _dlist_last_saved = NULL;
-    _dlist_state = DLIST_STATE_START;
+	_dlist_last_saved = NULL;
+	_dlist_state = DLIST_STATE_START;
 }
 
 static struct DLIST *dlist_iterator_next(void)
 {
-    switch(_dlist_state) {
-    case DLIST_STATE_START:
-        _dlist_last_node = (_dlist_last_node) ? _dlist_last_node : NULL;
-        _dlist_last_saved = _dlist_last_node;
-        _dlist_state = DLIST_STATE_NEXT;
-        break;
-    case DLIST_STATE_NEXT:
-        _dlist_last_node = (_dlist_last_node->next) ? _dlist_last_node->next : NULL;
-        if (!_dlist_last_node)
-            _dlist_state = DLIST_STATE_END;
-        else
-            _dlist_last_saved = _dlist_last_node;
-        break;
-    case DLIST_STATE_END:
-    default:
-        break;
-    }
-    return _dlist_last_node;
+	switch(_dlist_state) {
+		case DLIST_STATE_START:
+			_dlist_last_node = (_dlist_last_node) ? _dlist_last_node : NULL;
+			_dlist_last_saved = _dlist_last_node;
+			_dlist_state = DLIST_STATE_NEXT;
+		break;
+		case DLIST_STATE_NEXT:
+			_dlist_last_node = (_dlist_last_node->next)
+							? _dlist_last_node->next : NULL;
+			if (!_dlist_last_node)
+				_dlist_state = DLIST_STATE_END;
+			else
+				_dlist_last_saved = _dlist_last_node;
+		break;
+		case DLIST_STATE_END:
+		default:
+		break;
+	}
+	return _dlist_last_node;
 }
 
 static struct DLIST *dlist_iterator_last(void)
 {
-    return _dlist_last_saved;
+	return _dlist_last_saved;
 }
 
-void *_dlist_add_data(int num, const char *msg)
+static void *_dlist_add_data(int num, const char *msg)
 {
-    struct DLIST_DATA *data;
-    data = (struct DLIST_DATA *)malloc(sizeof(struct DLIST_DATA));
-    if (!data)
-        return NULL;
-    data->value = num;
-    data->message = strdup(msg);
-    return (void*)data;
+	struct DLIST_DATA *data;
+	data = (struct DLIST_DATA *)malloc(sizeof(struct DLIST_DATA));
+	if (!data)
+		return NULL;
+	data->value = num;
+	data->message = strdup(msg);
+	return (void*)data;
 }
 
-void _dlist_remove_data(void **dat)
+static void _dlist_remove_data(void **dat)
 {
-    struct DLIST_DATA *data = (struct DLIST_DATA *)*dat;
-    if (data->message)
-        free(data->message);
-    free(*dat);
-    *dat = NULL;
+	struct DLIST_DATA *data = (struct DLIST_DATA *)*dat;
+	if (data->message)
+		free(data->message);
+	free(*dat);
+	*dat = NULL;
 }
 
-static struct DLIST *dlist_create_list(int value, const char *message)
+static void _dlist_prepend_node(void **root, int num, const char *msg);
+static void _dlist_append_node(void **root, int num, const char *msg);
+
+static struct DLIST *dlist_init(add_data_cb add_func,
+                    remove_data_cb remove_func,
+                    prepend_node_cb prepend_func,
+                    append_node_cb append_func)
 {
-    struct DLIST *list;
-    list = (struct DLIST *)malloc(sizeof(struct DLIST));
-    if (!list)
-        return NULL;
-    list->count = 1;
-    list->data = dlist_add_data(value, message);
-    list->next = NULL;
-    if (!list->data) {
-        free(list);
-        return NULL;
-    }
-    return list;
+	dlist_add_data = (add_func) ? add_func : _dlist_add_data;
+	dlist_remove_data = (remove_func) ? remove_func : _dlist_remove_data;
+	dlist_prepend_node = (prepend_func) ? prepend_func : _dlist_prepend_node;
+	dlist_append_node = (append_func) ? append_func : _dlist_append_node;
+	if (!dlist_add_data || !dlist_remove_data ||
+		!dlist_prepend_node || !dlist_append_node) {
+		error_print("Cannot initialize functions.");
+		exit(1);
+	}
+	return NULL;
 }
 
 void _dlist_prepend_node(void **root, int num, const char *msg)
 {
-    struct DLIST *node, *tmp;
-    node = (struct DLIST *)malloc(sizeof(struct DLIST));
-    if (!node)
-        return;
-    ((struct DLIST *)*root)->count++;
-    tmp = (*root);
-    node->count = ((struct DLIST *)*root)->count;
-    node->data = dlist_add_data(num, msg);
-    node->next = tmp;
-    (*root) = node;
+	struct DLIST *node, *tmp;
+	node = (struct DLIST *)malloc(sizeof(struct DLIST));
+	if (!node)
+		return;
+	if (!(*root)) {
+		node->count = 1;
+		node->data = dlist_add_data(num, msg);
+		node->next = NULL;
+		(*root) = node;
+	} else {
+		((struct DLIST *)*root)->count++;
+		tmp = (*root);
+		node->count = ((struct DLIST *)*root)->count;
+		node->data = dlist_add_data(num, msg);
+		node->next = tmp;
+		(*root) = node;
+	}
 }
 
 void _dlist_append_node(void **root, int num, const char *msg)
 {
-    struct DLIST *node;
-    node = (struct DLIST *)malloc(sizeof(struct DLIST));
-    if (!node)
-        return;
-    dlist_iterator_init(*root);
-    while (dlist_iterator_next() != NULL);
-    ((struct DLIST *)*root)->count++;
-    node->count = ((struct DLIST *)*root)->count;
-    node->data = dlist_add_data(num, msg);
-    node->next = NULL;
-    dlist_iterator_last()->next = node;
+	struct DLIST *node;
+	node = (struct DLIST *)malloc(sizeof(struct DLIST));
+	if (!node)
+		return;
+	if (!(*root)) {
+		node->count = 1;
+		node->data = dlist_add_data(num, msg);
+		node->next = NULL;
+		(*root) = node;
+	} else {
+		struct DLIST *tmp = *root;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		((struct DLIST *)*root)->count++;
+		node->count = ((struct DLIST *)*root)->count;
+		node->data = dlist_add_data(num, msg);
+		node->next = NULL;
+		tmp->next = node;
+	}
 }
 
 static void dlist_destroy(struct DLIST **root)
 {
     if (*root != NULL) {
         struct DLIST *tmp;
-        dlist_iterator_init(*root);
-        while((tmp = dlist_iterator_next()) != NULL) {
+        tmp = *root;
+        while(tmp != NULL) {
             struct DLIST *next = tmp->next;
             dlist_remove_data(&(tmp->data));
             free(tmp);
-            tmp = NULL;
-            dlist_iterator_init(next);
+            tmp = next;
         }
         *root = NULL;
     }
--- main.c
+++ main.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
+#include "debug.h"
 #include "d_list.h"
 
-int main()
-{
-    struct DLIST *list, *tmp;
-
-    dlist_init(NULL, NULL, NULL, NULL);
-    list = dlist_create_list(1, "Philip");
-    dlist_prepend_node(&list, 2, "Robert");
-    dlist_prepend_node(&list, 3, "Dave");
-    dlist_prepend_node(&list, 4, "Mark");
-    dlist_append_node(&list, 5, "Allen");
-    dlist_iterator_init(list);
-    while ((tmp = dlist_iterator_next()) != NULL) {
-        printf("Hello, %s\n", ((struct DLIST_DATA *)tmp->data)->message);
-    }
-    if (!tmp)
-        printf("Nothing found in next slot\n");
-    printf("Last Item Data\n====================\n"
-           "Value: %d\nString: %s\n",
-           (((struct DLIST_DATA *)dlist_iterator_last()->data)->value),
-           (((struct DLIST_DATA *)dlist_iterator_last()->data)->message));
-    dlist_destroy(&list);
-    return 0;
+int main() {
+  struct DLIST *list1, *list2, *tmp;
+  list1 = dlist_init(NULL, NULL, NULL, NULL);
+  dlist_append_node(&list1, 1, "Philip");
+  dlist_append_node(&list1, 2, "Mark");
+  dlist_append_node(&list1, 3, "Mary");
+  list2 = dlist_init(NULL, NULL, NULL, NULL);
+  dlist_append_node(&list2, 4, "Bob");
+  dlist_append_node(&list2, 5, "Rob");
+  dlist_append_node(&list2, 6, "Tom");
+  dlist_iterator_init(list2);
+  while ((tmp = dlist_iterator_next()) != NULL) {
+    dlist_append_node(&list1,
+      ((struct DLIST_DATA*)tmp->data)->value,
+      ((struct DLIST_DATA*)tmp->data)->message);
+  }
+  dlist_destroy(&list2);
+  dlist_iterator_init(list1);
+  while ((tmp = dlist_iterator_next()) != NULL) {
+    printf("Value: %d\nName: %s\n",
+      ((struct DLIST_DATA*)tmp->data)->value,
+      ((struct DLIST_DATA*)tmp->data)->message);
+  }
+  dlist_destroy(&list1);
+  return 0;
 }
